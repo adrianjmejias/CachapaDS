2760b83418b37b69779d3a5d3c889d868ce112b9
adrianjmejias <adrianjmejias@gmail.com>
2019-07-31 15:04:43 -0400

    fibheap interface

--- CachapaDS/cds/FibHeap.h
+++ CachapaDS/cds/FibHeap.h
@@ -5,23 +5,28 @@
 namespace cds{
     template <class TT>
 	class FibHeap {
+		size_t size = 0;
+
+
+
+		class Node {
+			const TT &data;
+		};
+
+
+
+	public:
+		bool empty() const;
+		size_t size() const;
+		const TT& top() const;
+		void push(const TT&);
+
+		template <class... Args> void emplace(Args&&... args);
+		void pop();
+
 		
 
-
-
-
-
-
-		static void  RunTests();
 	};
 	
-	template<class TT>
-	void FibHeap<TT>::RunTests() {
-		using std::cout;
-		using std::endl;
 
-		cout << "Start tests"<<;
-
-	
-	}
 }
---
+++ Playground/src/main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <cds/FibHeap.h>
+
+
+
+
+struct Dummy {
+	std::string name;
+};
+
+int main() 
+{
+	cds::FibHeap<Dummy> fib;
+
+
+
+
+}
